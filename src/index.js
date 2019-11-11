102eae90e7417d6e1414d3c9e54a6906a7ba7e39
Michael P. Geraci <me@mgeraci.com>
2019-11-11 18:42:15 -0500

    restructure to a singleton; fix some linting errors

--- src/index.js
+++ src/index.js
@@ -1,338 +1,223 @@
-const ICON = "https://dropbox.michaelgeraci.com/public/fastmail/fastmail-icon.png";
-const NUMBERS = {
-	0: [
-		[0,1,1,1,0],
-		[1,0,0,0,1],
-		[1,0,0,0,1],
-		[1,0,0,0,1],
-		[1,0,0,0,1],
-		[1,0,0,0,1],
-		[0,1,1,1,0]
-	],
-	1: [
-		[0,1,1,0,0],
-		[1,0,1,0,0],
-		[0,0,1,0,0],
-		[0,0,1,0,0],
-		[0,0,1,0,0],
-		[0,0,1,0,0],
-		[1,1,1,1,1]
-	],
-	2: [
-		[0,1,1,1,0],
-		[1,0,0,0,1],
-		[0,0,0,0,1],
-		[0,0,0,1,0],
-		[0,0,1,0,0],
-		[0,1,0,0,0],
-		[1,1,1,1,1]
-	],
-	3: [
-		[0,1,1,1,0],
-		[1,0,0,0,1],
-		[0,0,0,0,1],
-		[0,0,1,1,0],
-		[0,0,0,0,1],
-		[1,0,0,0,1],
-		[0,1,1,1,0]
-	],
-	4: [
-		[0,0,0,1,0],
-		[0,0,1,1,0],
-		[0,1,0,1,0],
-		[1,0,0,1,0],
-		[1,1,1,1,1],
-		[0,0,0,1,0],
-		[0,0,0,1,0]
-	],
-	5: [
-		[1,1,1,1,1],
-		[1,0,0,0,0],
-		[1,1,1,0,0],
-		[0,0,0,1,0],
-		[0,0,0,0,1],
-		[1,0,0,0,1],
-		[0,1,1,1,0]
-	],
-	6: [
-		[0,1,1,1,0],
-		[1,0,0,0,1],
-		[1,0,0,0,0],
-		[1,1,1,1,0],
-		[1,0,0,0,1],
-		[1,0,0,0,1],
-		[0,1,1,1,0]
-	],
-	7: [
-		[1,1,1,1,1],
-		[0,0,0,0,1],
-		[0,0,0,0,1],
-		[0,0,0,1,0],
-		[0,0,0,1,0],
-		[0,0,1,0,0],
-		[0,0,1,0,0]
-	],
-	8: [
-		[0,1,1,1,0],
-		[1,0,0,0,1],
-		[1,0,0,0,1],
-		[0,1,1,1,0],
-		[1,0,0,0,1],
-		[1,0,0,0,1],
-		[0,1,1,1,0]
-	],
-	9: [
-		[0,1,1,1,0],
-		[1,0,0,0,1],
-		[1,0,0,0,1],
-		[0,1,1,1,1],
-		[0,0,0,0,1],
-		[1,0,0,0,1],
-		[0,1,1,1,0]
-	],
-	i: [
-		[0,0,0,0,0],
-		[0,0,1,0,0],
-		[0,0,1,0,0],
-		[0,0,1,0,0],
-		[0,0,1,0,0],
-		[0,0,1,0,0],
-		[0,0,0,0,0]
-	],
-	o: [
-		[0,0,0,0,0],
-		[0,0,1,0,0],
-		[0,1,0,1,0],
-		[0,1,0,1,0],
-		[0,1,0,1,0],
-		[0,0,1,0,0],
-		[0,0,0,0,0]
-	],
-	'+': [
-		[0,0,0,0,0],
-		[0,0,0,0,0],
-		[0,0,1,0,0],
-		[0,1,1,1,0],
-		[0,0,1,0,0],
-		[0,0,0,0,0],
-		[0,0,0,0,0]
-	]
+/* global document, Image */
+
+import { ICON, NUMBERS } from './constants';
+
+const FastmailFaviconCount = {
+  init() {
+    this.head = document.getElementsByTagName('head')[0];
+    this.timer = setInterval(() => {
+      this.poll();
+    }, 1000);
+    this.poll();
+
+    return true;
+  },
+
+  poll() {
+    if (this.getUnreadCount() >= 0) {
+      this.getUnreadCountIcon((icon) => {
+        this.setIcon(icon);
+      });
+    } else {
+      this.getIcon((icon) => {
+        this.setIcon(icon);
+      });
+    }
+  },
+
+  getUnreadCount() {
+    const folders = document.querySelectorAll('.v-FolderSource');
+    let res = 0;
+    const mailboxesToIgnore = ['Drafts', 'Trash'];
+
+    folders.forEach((folder, i) => {
+      const name = folder.querySelector('.v-FolderSource-name').innerHTML;
+      const count = parseInt(
+        folder.querySelector('.v-FolderSource-badge').innerHTML, 10,
+      ) || 0;
+
+      if (mailboxesToIgnore.indexOf(name) < 0) {
+        res += count;
+      }
+    });
+
+    return res;
+  },
+
+  drawUnreadCount(unread, callback) {
+    if (!this.textedCanvas) {
+      this.textedCanvas = [];
+    }
+
+    if (!this.textedCanvas[unread]) {
+      this.getUnreadCanvas((iconCanvas) => {
+        const textedCanvas = document.createElement('canvas');
+        textedCanvas.height = textedCanvas.width = iconCanvas.width;
+        const ctx = textedCanvas.getContext('2d');
+        ctx.drawImage(iconCanvas, 0, 0);
+
+        ctx.strokeStyle = 'rgba(255,255,255,0.5)';
+        ctx.fillStyle = 'rgba(255,255,255,0.7)';
+        ctx.fillStyle = '#000';
+
+        // how many digits the number of unread items is
+        const digits = String(unread).length;
+
+        // if greater than 99, set to '100+'
+        if (digits > 2) {
+          unread = 'ioo+';
+        }
+
+        let bgWidth = 0;
+        const padding = digits > 2 ? -1 : 1; // padding between digits
+        const topMargin = 7; // canvas height from icon top
+
+        // set the bg width
+        if (digits > 2) {
+          bgWidth = 14;
+        } else {
+          for (var index = 0; index < digits; index++) {
+            bgWidth += NUMBERS[String(unread)[index]][0].length;
+
+            if (index < digits - 1) {
+              bgWidth += padding;
+            }
+          }
+
+          bgWidth -= 1;
+        }
+
+        let digit;
+        let digitsWidth = bgWidth;
+
+        // stroke
+        for (var index = 0; index < digits; index++) {
+          digit = String(unread)[index];
+
+          if (NUMBERS[digit]) {
+            var map = NUMBERS[digit];
+            var height = map.length;
+            var width = map[0].length;
+
+            for (var y = 0; y < height; y++) {
+              for (var x = 0; x < width; x++) {
+                if (map[y][x]) {
+                  ctx.strokeRect(12 - digitsWidth + x, y + topMargin + 0, 3, 3);
+                }
+              }
+            }
+
+            digitsWidth -= width + padding;
+          }
+        }
+
+        // fill
+        digitsWidth = bgWidth;
+
+        for (var index = 0; index < digits; index++) {
+          digit = String(unread)[index];
+
+          if (NUMBERS[digit]) {
+            var map = NUMBERS[digit];
+            var height = map.length;
+            var width = map[0].length;
+
+            for (var y = 0; y < height; y++) {
+              for (var x = 0; x < width; x++) {
+                if (map[y][x]) {
+                  ctx.fillRect(13 - digitsWidth + x, y + topMargin + 1, 1, 1);
+                }
+              }
+            }
+
+            digitsWidth -= width + padding;
+          }
+        }
+
+        this.textedCanvas[unread] = textedCanvas;
+
+        callback(this.textedCanvas[unread]);
+      });
+    }
+
+    if (this.textedCanvas[unread]) {
+      callback(this.textedCanvas[unread]);
+    }
+  },
+
+  getIcon(callback) {
+    this.getUnreadCanvas((canvas) => {
+      callback(canvas.toDataURL('image/png'));
+    });
+  },
+
+  getUnreadCanvas(callback) {
+    if (!this.unreadCanvas) {
+      this.unreadCanvas = document.createElement('canvas');
+      this.unreadCanvas.height = this.unreadCanvas.width = 16;
+
+      const ctx = this.unreadCanvas.getContext('2d');
+      const img = new Image();
+
+      // allow cross-origin access for the image (since it's being hosted on
+      // michaelgeraci.com, but displaying on fastmail.com
+      img.crossOrigin = 'anonymous';
+
+      img.addEventListener('load', () => {
+        ctx.drawImage(img, 0, 0);
+        callback(this.unreadCanvas);
+      }, true);
+
+      img.src = ICON;
+    } else {
+      callback(this.unreadCanvas);
+    }
+  },
+
+  getUnreadCountIcon(callback) {
+    const unread = this.getUnreadCount();
+
+    this.drawUnreadCount(unread, (icon) => {
+      callback(icon.toDataURL('image/png'));
+    });
+  },
+
+  setIcon(icon) {
+    const links = this.head.getElementsByTagName('link');
+
+    // remove the old icon
+    for (let i = 0; i < links.length; i++) {
+      const link = links[i];
+
+      if (
+        (link.rel === 'shortcut icon' || link.rel === 'icon')
+        && link.href !== icon
+      ) {
+        this.head.removeChild(link);
+      } else if (link.href === icon) {
+        return;
+      }
+    }
+
+    const newIcon = document.createElement('link');
+    newIcon.type = 'image/png';
+    newIcon.rel = 'shortcut icon';
+    newIcon.href = icon;
+    this.head.appendChild(newIcon);
+
+    // Chrome hack for updating the favicon
+    const shim = document.createElement('iframe');
+    shim.width = shim.height = 0;
+    document.body.appendChild(shim);
+    shim.src = 'icon';
+    document.body.removeChild(shim);
+  },
 };
 
-(function FastmailFaviconCount() {
-	var self = this;
-
-	this.construct = () => {
-		this.head = document.getElementsByTagName("head")[0];
-		this.timer = setInterval(this.poll, 500);
-		this.poll();
-
-		return true;
-	};
-
-	this.poll = () => {
-		if (self.getUnreadCount() >= 0) {
-			self.getUnreadCountIcon(function(icon) {
-				self.setIcon(icon);
-			});
-		} else {
-			self.getIcon(function(icon) {
-				self.setIcon(icon);
-			});
-		}
-	};
-
-	this.getUnreadCount = () => {
-		const folders = document.querySelectorAll(".v-FolderSource");
-		let res = 0;
-		const mailboxesToIgnore = ["Drafts", "Trash"];
-
-		folders.forEach((folder, i) => {
-			const name = folder.querySelector(".v-FolderSource-name").innerHTML;
-			const count = parseInt(
-				folder.querySelector(".v-FolderSource-badge").innerHTML, 10
-			) || 0;
-
-			if (mailboxesToIgnore.indexOf(name) < 0) {
-				res += count;
-			}
-		});
-
-		return res;
-	}
-
-	this.drawUnreadCount = (unread, callback) => {
-		if (!self.textedCanvas) {
-			self.textedCanvas = [];
-		}
-
-		if (!self.textedCanvas[unread]) {
-			self.getUnreadCanvas(function(iconCanvas) {
-				const textedCanvas = document.createElement("canvas");
-				textedCanvas.height = textedCanvas.width = iconCanvas.width;
-				const ctx = textedCanvas.getContext("2d");
-				ctx.drawImage(iconCanvas, 0, 0);
-
-				ctx.strokeStyle = "rgba(255,255,255,0.5)"
-				ctx.fillStyle = "rgba(255,255,255,0.7)";
-				ctx.fillStyle = "#000";
-
-				// how many digits the number of unread items is
-				const count = String(unread).length;
-
-				// if greater than 99, set to "100+"
-				if (count > 2) {
-					unread = "ioo+";
-				}
-
-				let bgWidth = 0;
-				const padding = count > 2 ? -1 : 1; // padding between digits
-				const topMargin = 7; // canvas height from icon top
-
-				// set the bg width
-				if (count > 2) {
-					bgWidth = 14
-				} else {
-					for (var index = 0; index < count; index++) {
-						bgWidth += NUMBERS[String(unread)[index]][0].length;
-
-						if (index < count-1) {
-							bgWidth += padding;
-						}
-					}
-
-					bgWidth -= 1;
-				}
-
-				let digit;
-				let digitsWidth = bgWidth;
-
-				// stroke
-				for (var index = 0; index < count; index++) {
-					digit = String(unread)[index];
-
-					if (NUMBERS[digit]) {
-						var map = NUMBERS[digit];
-						var height = map.length;
-						var width = map[0].length;
-
-						for (var y = 0; y < height; y++) {
-							for (var x = 0; x < width; x++) {
-								if (map[y][x]) {
-									ctx.strokeRect(12 - digitsWidth + x, y + topMargin + 0, 3, 3);
-								}
-							}
-						}
-
-						digitsWidth -= width + padding;
-					}
-				}
-
-				// fill
-				digitsWidth = bgWidth;
-
-				for (var index = 0; index < count; index++) {
-					digit = String(unread)[index];
-
-					if (NUMBERS[digit]) {
-						var map = NUMBERS[digit];
-						var height = map.length;
-						var width = map[0].length;
-
-						for (var y = 0; y < height; y++) {
-							for (var x = 0; x < width; x++) {
-								if(map[y][x]) {
-									ctx.fillRect(13 - digitsWidth + x, y+topMargin+1, 1, 1);
-								}
-							}
-						}
-
-						digitsWidth -= width + padding;
-					}
-				}
-
-				self.textedCanvas[unread] = textedCanvas;
-
-				callback(self.textedCanvas[unread]);
-			});
-		}
-
-		if (self.textedCanvas[unread]){
-			callback(self.textedCanvas[unread]);
-		}
-	}
-
-	this.getIcon = (callback) => {
-		self.getUnreadCanvas(function(canvas) {
-			callback(canvas.toDataURL("image/png"));
-		});
-	}
-
-	this.getUnreadCanvas = (callback) => {
-		if(!self.unreadCanvas) {
-			self.unreadCanvas = document.createElement("canvas");
-			self.unreadCanvas.height = self.unreadCanvas.width = 16;
-
-			var ctx = self.unreadCanvas.getContext("2d");
-			var img = new Image();
-
-			// allow cross-origin access for the image (since it's being hosted on
-			// michaelgeraci.com, but displaying on fastmail.com
-			img.crossOrigin = "anonymous";
-
-			img.addEventListener("load", () => {
-				ctx.drawImage(img, 0, 0);
-				callback(self.unreadCanvas);
-			}, true);
-
-			img.src = ICON;
-		} else {
-			callback(self.unreadCanvas);
-		}
-	}
-
-	this.getUnreadCountIcon = (callback) => {
-		const unread = self.getUnreadCount();
-
-		self.drawUnreadCount(unread, (icon) => {
-			callback(icon.toDataURL('image/png'));
-		});
-	}
-
-	this.setIcon = (icon) => {
-		const links = self.head.getElementsByTagName("link");
-
-		// remove the old icon
-		for (let i=0; i < links.length; i++) {
-			const link = links[i];
-
-			if (
-				(link.rel == "shortcut icon" || link.rel=="icon") &&
-				link.href != icon
-			) {
-				self.head.removeChild(link);
-			} else if (link.href == icon) {
-				return;
-			}
-		};
-
-		const newIcon = document.createElement("link");
-		newIcon.type = "image/png";
-		newIcon.rel = "shortcut icon";
-		newIcon.href = icon;
-		self.head.appendChild(newIcon);
-
-		// Chrome hack for updating the favicon
-		const shim = document.createElement('iframe');
-		shim.width = shim.height = 0;
-		document.body.appendChild(shim);
-		shim.src = "icon";
-		document.body.removeChild(shim);
-	}
-
-	this.toString = () => {
-		return '[object FastmailFaviconCount]';
-	}
-
-	return this.construct();
-}());
+// run the initiliazer with a self-executing function
+(() => {
+  // delay initial action for fastmail to initialize
+  setTimeout(() => {
+    FastmailFaviconCount.init();
+  }, 500);
+})();
